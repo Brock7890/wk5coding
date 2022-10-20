b1746d41c1192dffd421eee473d6c45b3b05a44e
Brcok7890 <nm1107@student.aru.ac.uk>
2022-10-20 09:55:36 +0100

    v1.0 - circCalc

--- Program.cs
+++ Program.cs
@@ -1,22 +1,13 @@
-﻿Console.WriteLine("Enter the size of your array");
-int arraySize = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[arraySize];
-int big = 0;
-int small = 2147483647;
-Console.WriteLine($"Please enter {arraySize} numbers");
-for (int i = 0; i < arraySize; i++)
-{
-    Console.Write($"{i + 1}: ");
-    array[i] = Convert.ToInt32(Console.ReadLine());
-    if (array[i] > big)
-    {
-        big = array[i];
-    }
-    else if (array[i] < small)
-    {
-        small = array[i];
-    }
-}
+﻿Console.WriteLine("Welcome to method testing.");
+int radius;
+double area;
 
-Console.WriteLine($"{big} is the biggest number in your array, and {small} is the smallest number in the array");
+void circleAreaCalculator()
+{
+    Console.Write("Enter the radius of a circle: ");
+    radius = Convert.ToInt32(Console.ReadLine());
+    area = Math.PI * (radius * radius);
+    Console.WriteLine($"The area of this circle is {area.ToString("0.0000")}");
+}
+circleAreaCalculator();
 Console.ReadKey();
